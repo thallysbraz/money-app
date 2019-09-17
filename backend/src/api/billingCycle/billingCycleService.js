1d368a8874324b45f0d67c699c27363312469d60
thallysbraz <thallysbraz3@gmail.com>
2019-09-17 11:45:57 -0300

    pegando quantidade de registros

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -5,4 +5,14 @@ BillingCycle.methods(["get", "post", "put", "delete"]);
 //forçando retornar o objeto atualizado e rodando validações no "put"
 BillingCycle.updateOptions({ new: true, runValidators: true });
 
+BillingCycle.route("count", (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] });
+    } else {
+      res.json({ value });
+    }
+  });
+});
+
 module.exports = BillingCycle;
