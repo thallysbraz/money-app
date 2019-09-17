c8751d54c605dd74f2f5d39f7f50404e6df6dec7
thallysbraz <thallysbraz3@gmail.com>
2019-09-17 12:28:21 -0300

    tratando mensagens de erros

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,9 +1,11 @@
 const BillingCycle = require("./billingCycle");
+const errorHandler = require("../common/errorHandler");
 
 //web service pega os metodos do http aplica em uma mesma url o metodo requisitado.
 BillingCycle.methods(["get", "post", "put", "delete"]);
 //forçando retornar o objeto atualizado e rodando validações no "put"
 BillingCycle.updateOptions({ new: true, runValidators: true });
+BillingCycle.after("post", errorHandler).after("put", errorHandler);
 
 BillingCycle.route("count", (req, res, next) => {
   BillingCycle.count((error, value) => {
@@ -44,20 +46,5 @@ BillingCycle.route("summary", (req, res, next) => {
     }
   );
 });
-/*
-BillingCycle.route('summary', (req, res, next) => {
-    BillingCycle.aggregate([{ 
-        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
-    }, { 
-        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
-    }, { 
-        $project: {_id: 0, credit: 1, debt: 1}
-    }], (error, result) => {
-        if(error) {
-            res.status(500).json({errors: [error]})
-        } else {
-            res.json(result[0] || {credit: 0, debt: 0})
-        }
-    })
-})*/
+
 module.exports = BillingCycle;
